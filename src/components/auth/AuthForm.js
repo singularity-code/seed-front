eaa2c8be64016582e0612f078321e96245c9d5af
chris.lee <neoxrock@gmail.com>
2022-01-06 23:51:09 +1100

    login form

--- src/components/auth/AuthForm.js
+++ src/components/auth/AuthForm.js
@@ -1,8 +1,58 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import palette from '../../lib/styles/palette';
+import Button from '../common/Button';
 
-const AuthFormBlock = styled.div``;
+const AuthFormBlock = styled.div`
+	h3 {
+		margin: 0;
+		color: ${palette.gray[8]};
+		margin-bottom: 1rem;
+	}
+`;
+
+const StyledInput = styled.input`
+	font-size: 1rem;
+	border: none;
+	border-bottom: 1px solid ${palette.gray[5]};
+	padding-bottom: 0.5rem;
+	outline: none;
+	width: 100%;
+	&:focus {
+		color: $oc-teal-7;
+		border-bottom: 1px solid ${palette.gray[7]};
+	}
+	& + & {
+		margin-top: 1rem;
+	}
+`;
+
+const Footer = styled.div`
+	margin-top: 2rem;
+	text-align: right;
+	a {
+		color: ${palette.gray[6]};
+		text-decoration: underline;
+		&:hover {
+			color: ${palette.gray[9]};
+		}
+	}
+`;
+
 const AuthForm = () => {
-	return <AuthFormBlock>AuthFormBlock</AuthFormBlock>;
+	return (
+		<AuthFormBlock>
+			<h3>Login</h3>
+			<form>
+				<StyledInput autoComplete="username" name="username" placeholder="username" />
+				<StyledInput autoComplete="new-password" name="password" placeholder="Password" type="password" />
+				<Button>LOGIN</Button>
+			</form>
+			<Footer>
+				<Link to="/signup">REGISTER</Link>
+			</Footer>
+		</AuthFormBlock>
+	);
 };
 
 export default AuthForm;
